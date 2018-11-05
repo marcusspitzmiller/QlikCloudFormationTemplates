587d004eefd431906de14834cacdd2c7ba0cda85
Marcus Spitzmiller <MSI@qlik.com>
2018-11-05 13:11:20 -0600

    added Sept2018

--- Scripts/cn-install.ps1
+++ Scripts/cn-install.ps1
@@ -36,6 +36,11 @@ function Write-Log
 $json = @{
     qliksense = @(
         @{
+            name= "Qlik Sense September 2018 Patch 1"
+            url= "https://da3hntz84uekx.cloudfront.net/QlikSense/12.36/1/_MSI/Qlik_Sense_update.exe"
+            url2= "https://da3hntz84uekx.cloudfront.net/QlikSense/12.36/0/_MSI/Qlik_Sense_setup.exe"
+        },
+        @{
             name= "Qlik Sense November 2017 Patch 1"
             url= "https://da3hntz84uekx.cloudfront.net/QlikSense/11.24/1/_MSI/Qlik_Sense_update.exe"
             url2= "https://da3hntz84uekx.cloudfront.net/QlikSense/11.24/0/_MSI/Qlik_Sense_setup.exe"
